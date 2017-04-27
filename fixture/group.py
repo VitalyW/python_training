847a722f574793daea6cdf48d8395fe0231d0b9b
VitalyW <vitaly@Vitalys-MacBook-Pro.local>
2017-04-26 20:39:26 -0500

    Добавил общую фикстуру

--- fixture/group.py
+++ fixture/group.py
@@ -31,3 +31,10 @@ class GroupHelper:
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_css_selector("[name='selected[]']").click()
+        wd.find_element_by_css_selector("[name='delete']").click()
+        self.return_to_groups_page()
