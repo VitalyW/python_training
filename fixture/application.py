71a1d9854382329bcbbf7730d6fca1091ba5872f
VitalyW <vitaly@Vitalys-MacBook-Pro.local>
2017-05-20 18:57:52 -0500

    Реализовал опции командной строки, для типа браузера и страницы приложения

--- fixture/application.py
+++ fixture/application.py
@@ -7,12 +7,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.binary = FirefoxBinary("/Users/vitaly/Documents/Firefox_esr_45/Firefox.app/Contents/MacOS/firefox")
-        self.wd = webdriver.Firefox(capabilities={"marionette": False}, firefox_binary=self.binary)
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.binary = FirefoxBinary("/Users/vitaly/Documents/Firefox_esr_45/Firefox.app/Contents/MacOS/firefox")
+            self.wd = webdriver.Firefox(capabilities={"marionette": False}, firefox_binary=self.binary)
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -25,7 +33,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("/index.php") and "Number of results:" in
                                                         wd.find_element_by_css_selector("[style='width:24em;']").text):
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
     def return_to_home_page(self):
         wd = self.wd
