eb17de4db0a53c97381e19b47884f2c6deb80136
VitalyW <vitaly@Vitalys-MacBook-Pro.local>
2017-05-23 00:44:07 -0500

    Фильтрую удаление пробела в номере телефона

--- test/test_random_contact.py
+++ test/test_random_contact.py
@@ -19,7 +19,8 @@ def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+                                       filter(lambda x: x.strip('\n'),
+                                              [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone])))))
 
 
 def merge_emails_like_on_home_page(contact):
