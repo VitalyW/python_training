5d01a5d9c434038319e87189226d96c98ebfd3a7
VitalyW <vitaly@vitalys-mbp.attlocal.net>
2017-06-19 22:24:55 -0500

    Проблема с тестами для модификации

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -15,24 +15,3 @@ def test_modify_group_name(app, db, check_ui):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
-
-# def test_modify_group_footer(app):
-#     if app.group.count() == 0:
-#         app.group.create(Group(header="modify this header"))
-#     old_groups = app.group.get_group_list()
-#     group = Group(footer="modified footer")
-#     group.id = old_groups[0].id
-#     app.group.modify_first_group(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
-#     old_groups[0] = group
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-#
-# def test_modify_group_header(app):
-#     if app.group.count() == 0:
-#         app.group.create(Group(footer="modify this footer"))
-#     old_groups = app.group.get_group_list()
-#     app.group.modify_first_group(Group(header="modify header"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
